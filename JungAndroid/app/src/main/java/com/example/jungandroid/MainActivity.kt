26a1463050da5772ebc26ef4d68471ead5022180
kjh <rlawjdgjs73@gmail.com>
2022-02-27 16:31:56 +0900

    라이프사이클, 그라데이션(작동안됨)

--- JungAndroid/app/src/main/java/com/example/jungandroid/MainActivity.kt
+++ JungAndroid/app/src/main/java/com/example/jungandroid/MainActivity.kt
@@ -2,10 +2,37 @@ package com.example.jungandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+
+    val TAG: String = "로그"
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d(TAG, "MainActivity- onCreate() called")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "MainActivity- onStart() called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        Log.d(TAG, "MainActivity- onResume() called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "MainActivity- onStop() called")
+    }
+
+    override fun onDestroy() {
+        Log.d(TAG, "MainActivity- onDestroy() called")
+        super.onDestroy()
     }
 }
