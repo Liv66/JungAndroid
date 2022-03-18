40babeca3272988fd6ebad6703f4da9c1d488900
Liv66 <rlawjdgjs73@gmail.com>
2022-03-18 18:31:33 +0900

    - 서치뷰 1. 메뉴 아이템에 app:actionViewClass="androidx.appcompat.widget.SearchView" 추가 app:showAsAction="ifRoom|collapseActionView"설정해준다.
    2. 액티비티에서
    setSupportActionBar(앱바객체) 한다.
    
    3. 서치뷰의 객체는 androidx의 SearchView로 설정해준다.
    this.findViewById(androidx.appcompat.R.id.search_src_text) // 서치뷰 텍스트
    override fun onCreateOptionsMenu(menu: Menu?): Boolean 구현해서 Menu 설정한다.
    
    4. 액티비티에서 SearchView.onQueryTextListener을 구현
    override fun onQueryTextSubmit(query: String?): Boolean //Submit 할 때
    override fun onQueryTextChange(newText: String?): Boolean // 타이핑할 때
    
    -액티비티에서 위젯의 리스너 이벤트 설정하기(인터페이스 구현과 객체에서 설정하는 방법 무슨 차이있나?)
    1. 액티비티에서 CompoundButton.OnCheckedChangeListener(Switch 리스너), View.OnClickListener(Button 리스너)
    등등 구현한다.
    2.  override fun onCheckedChanged(switch: CompoundButton?, isChecked: Boolean)
    override fun onClick(view: View?)로 이벤트 설정해준다.
    3.      searchHistorySwitch.setOnCheckedChangeListener(this)
            searchHistoryButton.setOnClickListener(this) 본문에서 바인딩 해준다.
    
    - 커스텀스타일 생성
    theme에서 style을 직접 생성한다.

--- JungAndroid/app/src/main/java/com/example/jungandroid/PhotoCollectionActivity.kt
+++ JungAndroid/app/src/main/java/com/example/jungandroid/PhotoCollectionActivity.kt
@@ -1,27 +1,41 @@
 package com.example.jungandroid
 
+
+import android.app.SearchManager
+import android.content.Context
+import android.graphics.Color
 import android.os.Bundle
-import android.os.PersistableBundle
+import android.text.InputFilter
 import android.util.Log
-import android.widget.TextView
-import android.widget.Toolbar
+import android.view.Menu
+import android.view.View
+import android.widget.*
+import androidx.appcompat.widget.SearchView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.jungandroid.model.Photo
 import com.example.jungandroid.recyclerview.PhotoGridRecyclerViewAdapter
 import com.example.jungandroid.utills.Constants.TAG
-import com.google.android.material.appbar.AppBarLayout
 import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.switchmaterial.SwitchMaterial
 
-class PhotoCollectionActivity : AppCompatActivity() {
+class PhotoCollectionActivity : AppCompatActivity(), SearchView.OnQueryTextListener,
+    CompoundButton.OnCheckedChangeListener, View.OnClickListener {
 
     //데이터
     private var photoList = ArrayList<Photo>()
+
     //어뎁터
     private lateinit var photoGridRecyclerViewAdapter: PhotoGridRecyclerViewAdapter
-    //리사이클러뷰
+
     private lateinit var myRecyclerView: RecyclerView
+    private lateinit var topAppBar: MaterialToolbar
+    private lateinit var mySearchView: SearchView
+    private lateinit var mySearchViewEditText: EditText
+    private lateinit var linearSeaerchLayout: LinearLayout
+    private lateinit var searchHistoryButton: Button
+    private lateinit var searchHistorySwitch: SwitchMaterial
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,14 +45,22 @@ class PhotoCollectionActivity : AppCompatActivity() {
         val bundle = intent.getBundleExtra("array_bundle")
 
         val searchTerm = intent.getStringExtra("search_term")
-        photoList = bundle?.getSerializable("photo_array_list") as ArrayList<Photo>
 
-        findViewById<MaterialToolbar>(R.id.topAppBar).title = searchTerm
+        photoList = bundle?.getSerializable("photo_array_list") as ArrayList<Photo>
+        topAppBar = findViewById<MaterialToolbar>(R.id.topAppBar)
+        linearSeaerchLayout = findViewById(R.id.linear_search_history_view)
+        searchHistoryButton = findViewById(R.id.search_history_button)
+        searchHistorySwitch = findViewById(R.id.search_history_mode_switch)
 
         Log.d(
             TAG,
             "PhotoCollectionActivity - onCreate() called / searchTerm : $searchTerm, photoList.count() : ${photoList.count()}"
         )
+
+        searchHistorySwitch.setOnCheckedChangeListener(this)
+        searchHistoryButton.setOnClickListener(this)
+        topAppBar.title = searchTerm
+        setSupportActionBar(topAppBar)
 
         this.photoGridRecyclerViewAdapter = PhotoGridRecyclerViewAdapter()
         this.photoGridRecyclerViewAdapter.submitList(photoList)
@@ -48,5 +70,97 @@ class PhotoCollectionActivity : AppCompatActivity() {
             GridLayoutManager(this, 2, GridLayoutManager.VERTICAL, false)
         myRecyclerView.adapter = photoGridRecyclerViewAdapter
 
+    }
+
+    //앱바 작동하게 설정해줌
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        Log.d(TAG, "PhotoCollectionActivity - onCreateOptionsMenu() called")
+
+        val inflater = menuInflater
+        inflater.inflate(R.menu.top_app_bar_menu, menu)
+
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        this.mySearchView = menu?.findItem(R.id.search_menu_item)?.actionView as SearchView
+        this.mySearchView.apply {
+            this.queryHint = "검색어를 입력해주세요"
+
+            this.setOnQueryTextListener(this@PhotoCollectionActivity)
+
+            this.setOnQueryTextFocusChangeListener { _, hasExpaned ->
+                when (hasExpaned) {
+                    true -> {
+                        Log.d(TAG, "PhotoCollectionActivity - 서치뷰 열림")
+                        linearSeaerchLayout.visibility = View.VISIBLE
+                    }
+                    false -> {
+                        Log.d(TAG, "PhotoCollectionActivity - 서치뷰 닫힘")
+                        linearSeaerchLayout.visibility = View.INVISIBLE
+                    }
+                }
+            }
+
+            //서치뷰에서 에딧 텍스트를 가져온다
+            mySearchViewEditText = this.findViewById(androidx.appcompat.R.id.search_src_text)
+        }
+
+        this.mySearchViewEditText.apply {
+            this.filters = arrayOf(InputFilter.LengthFilter(12))
+            this.setTextColor(Color.WHITE)
+            this.setHintTextColor(Color.WHITE)
+        }
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+
+    //서치뷰 검색 입력 이벤트
+    //검색버튼이 클릭되었을 때
+    override fun onQueryTextSubmit(query: String?): Boolean {
+        Log.d(TAG, "PhotoCollectionActivity - onQueryTextSubmit() called / query : $query")
+
+        if (!query.isNullOrEmpty()) {
+            this.topAppBar.title = null
+
+            //TODO : api 호출
+            //TODO : 검색어 저장
+        }
+        this.mySearchView.setQuery("", false)
+        this.mySearchView.clearFocus()
+        this.topAppBar.collapseActionView()
+
+        return true
+    }
+
+    override fun onQueryTextChange(newText: String?): Boolean {
+        Log.d(TAG, "PhotoCollectionActivity - onQueryTextChange() called / newText : $newText")
+
+        val userInputText = newText ?: ""
+
+        if (userInputText.count() == 12) {
+            Toast.makeText(this, "검색어는 12자까지 가능합니다", Toast.LENGTH_SHORT).show()
+        }
+
+        return true
+    }
+
+    //스위치 버튼 클릭 감지
+    override fun onCheckedChanged(switch: CompoundButton?, isChecked: Boolean) {
+        when(switch){
+            searchHistorySwitch -> {
+                if(isChecked){
+                    Log.d(TAG, "검색어 저장 기능 on")
+                } else {
+                    Log.d(TAG, "검색어 저장 기능 off")
+                }
+            }
+        }
+    }
+
+    override fun onClick(view: View?) {
+        when(view){
+            searchHistoryButton -> {
+                Log.d(TAG, "검색 기록 삭제 버튼이 클릭 되었따.")
+            }
+        }
     }
 }
